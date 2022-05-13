041dbc960ee8a9d65685589e98346bda4b197cfd
Daniel Sepúlveda <d.sepulveda.perez@gmail.com>
2022-05-13 10:30:31 -0400

    Router added and Page components created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Home from './pages/Home'
+import Country from './pages/Country'
+import NotFound from './pages/NotFound'
 
 const App = () => {
     return (
-        <div>App</div>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/country' element={<Country />} />
+                <Route path='/not-found' element={<NotFound />} />
+                <Route path='*' element={<Navigate to='/not-found' />} />
+            </Routes>
+        </BrowserRouter>
     )
 }
 
