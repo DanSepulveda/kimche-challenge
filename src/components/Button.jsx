076010d8e936894c93df19bd36d44054241871b9
Daniel Sepúlveda <d.sepulveda.perez@gmail.com>
2022-05-13 11:31:09 -0400

    Button component: styles added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+const Button = ({ children, onClick, active }) => {
+    const color = active
+        ? ' bg-teal-600 pointer-events-none'
+        : ' bg-slate-300/50 hover:bg-slate-300/70 transition-all duration-300 text-teal-700'
 
-const Button = ({ children }) => {
     return (
-        <button>{children}</button>
+        <button
+            type='button'
+            onClick={onClick}
+            className={`text-white w-36 mx-4 text-lg py-1 px-3 select-none rounded${color}`}
+        >
+            {children}
+        </button>
     )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,20 @@ import useTitle from '../hooks/useTitle'
 import H1 from '../components/H1'
 import CardsGrid from '../components/CardsGrid'
 import Button from '../components/Button'
+import { useState } from 'react'
 
 const Home = () => {
     useTitle('Countries | Home')
+    const [goupBy, setGoupBy] = useState('continent')
+
+
+    const inputHandler = (event) => {
+        console.log(event.target.value)
+    }
+
+    const buttonHandler = (label) => {
+        setGoupBy(label)
+    }
 
     return (
         <div>
@@ -12,11 +23,20 @@ const Home = () => {
             <p className='text-center'>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, tempore! Alias, porro odio! Soluta nam, neque earum, voluptates fugiat veritatis possimus enim sint modi, ipsa ipsam recusandae cumque doloribus qui
             </p>
-            <input type='text' name='country' />
-            <div className='flex'>
-                <h2>Grouped by</h2>
-                <Button>Botón 1</Button>
-                <Button>Botón 2</Button>
+            <input
+                type='text'
+                name='country'
+                className='w-75'
+                onChange={inputHandler}
+            />
+            <div className='flex items-center'>
+                <p className='font-medium text-xl mr-10'>Grouped by</p>
+                <Button onClick={() => buttonHandler('continent')} active={goupBy === 'continent'}>
+                    Continent
+                </Button>
+                <Button onClick={() => buttonHandler('language')} active={goupBy === 'language'}>
+                    Language
+                </Button>
             </div>
             <CardsGrid />
         </div>
