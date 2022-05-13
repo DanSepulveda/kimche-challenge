14ce5b5b1eebb6f050769a91cb4df6d1da907a66
Daniel Sepúlveda <d.sepulveda.perez@gmail.com>
2022-05-13 10:37:07 -0400

    useTitle hook created

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import useTitle from '../hooks/useTitle'
 
 const Country = () => {
+    useTitle('Countries | X')
+
     return (
         <div>Country</div>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import useTitle from '../hooks/useTitle'
 
 const Home = () => {
+    useTitle('Countries | Home')
+
     return (
         <div>Home</div>
     )
