baac1b4dfcdd4848376fa94eca5af39b9b9f055b
Daniel Sepúlveda <d.sepulveda.perez@gmail.com>
2022-05-17 13:25:31 -0400

    Apollo client created with @apollo dependency

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import './index.css'
 import App from './App'
 import ReactDOM from 'react-dom/client'
-import ApolloClient from 'apollo-boost'
-import { ApolloProvider } from '@apollo/react-hooks'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 const client = new ApolloClient({
-  uri: 'https://countries.trevorblades.com/'
+  uri: 'https://countries.trevorblades.com/',
+  cache: new InMemoryCache
 })
 
 root.render(
