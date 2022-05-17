e5fd9519b035b88b7da9adffe531130f26991fca
Daniel Sepúlveda <d.sepulveda.perez@gmail.com>
2022-05-17 18:32:51 -0400

    Font size and color changed

--- src/components/H1.jsx
+++ src/components/H1.jsx
@@ -1,6 +1,6 @@
 const H1 = ({ children }) => {
     return (
-        <h1 className='text-center text-5xl text-teal-600 font-bold mb-10'>
+        <h1 className='text-center text-6xl text-teal-300/80 font-bold mb-7'>
             {children}
         </h1>
     )
