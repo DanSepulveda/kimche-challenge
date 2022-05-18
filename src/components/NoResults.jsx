47662f254b63a300f0d7e7d28466e802e9e30dbf
Daniel Sepúlveda <d.sepulveda.perez@gmail.com>
2022-05-18 01:26:31 -0400

    Messages and icons added

--- src/components/NoResults.jsx
+++ src/components/NoResults.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { useSelector } from 'react-redux'
+import { countryState } from '../redux/countrySlice'
+import { RiArrowUpLine, RiEmotionSadLine } from 'react-icons/ri'
 
 const NoResults = () => {
+    const { inputEmpty } = useSelector(countryState)
+
+    const message = inputEmpty
+        ? 'Start writing'
+        : 'No results'
+
+    const icon = inputEmpty
+        ? <RiArrowUpLine className='text-6xl' />
+        : <RiEmotionSadLine className='text-6xl' />
+
     return (
-        <div>NoResults</div>
+        <div className='mt-10 bg-slate-100 h-48 w-11/12 md:w-1/2 lg:w-1/3 rounded-lg flex flex-col items-center justify-center shadow text-gray-600'>
+            {icon}
+            <p className='text-3xl font-bold'>
+                {message}
+            </p>
+        </div>
     )
 }
 
