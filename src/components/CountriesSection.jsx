b9d815da4b991a5ac335935266d70c7898f08605
Daniel Sepúlveda <d.sepulveda.perez@gmail.com>
2022-05-17 23:33:44 -0400

    Rendering CountryCard component feature added

--- src/components/CountriesSection.jsx
+++ src/components/CountriesSection.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import CountryCard from './CountryCard'
 
-const CountriesSection = () => {
+const CountriesSection = ({ group }) => {
+    const title = group[0]
+    const countries = group[1]
+
     return (
-        <div>CountriesSection</div>
+        <div className='mb-20'>
+            <h3 className='text-4xl text-center uppercase bg-red-800 py-12 text-gray-300/90 font-bold'>
+                {title}
+            </h3>
+            <div className='p-10 gap-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4'>
+                {countries.map(country => <CountryCard country={country} key={country.name} />)}
+            </div>
+        </div>
     )
 }
 
