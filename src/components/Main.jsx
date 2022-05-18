60b4ab1b6a44726d6fbbb521c9d738669c151431
Daniel Sepúlveda <d.sepulveda.perez@gmail.com>
2022-05-18 02:06:41 -0400

    Footer added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,6 +2,9 @@ const Main = ({ children }) => {
     return (
         <main className='bg-gray-100 min-h-screen w-full flex flex-col'>
             {children}
+            <footer className='bg-gray-800 py-2'>
+                <p className='text-center text-slate-100'>Developed by Daniel Sepúlveda</p>
+            </footer>
         </main>
     )
 }
