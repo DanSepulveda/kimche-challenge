3a4ae1f4bbbf4a4cc6f7dda44cd0578bb79dbcd6
Daniel Sepúlveda <d.sepulveda.perez@gmail.com>
2022-05-17 23:30:45 -0400

    CountryCard created and styles added

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import Detail from './Detail'
+import { useSelector } from 'react-redux'
+import { countryState } from '../redux/countrySlice'
+import getLangList from '../utils/getLangList'
 
-const CountryCard = () => {
+const CountryCard = ({ country }) => {
+    const { name, capital, code, continent, currency, emoji, emojiU, languages, native } = country
+    const { groupBy } = useSelector(countryState)
+
     return (
-        <div>CountryCard</div>
+        <div className='rounded-lg px-5 py-10 shadow bg-slate-100'>
+            <p className='text-5xl text-center'>{emoji}</p>
+            <h4 className='text-center text-xl my-4 font-ubuntu'>{name}</h4>
+            <Detail title='Original' content={native} />
+            <Detail title='Capital' content={capital} />
+            <Detail title='Currency' content={currency} />
+            <Detail title='Code' content={code} />
+            {groupBy !== 'continent' && <Detail title='Continent' content={continent.name} />}
+            {groupBy === 'continent' && <Detail title='Languages' content={getLangList(languages)} />}
+        </div>
     )
 }
 
