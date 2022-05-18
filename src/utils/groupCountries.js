6c2ad69cb4b753bfb7b783eda663685b7f61e6c1
Daniel Sepúlveda <d.sepulveda.perez@gmail.com>
2022-05-17 21:39:37 -0400

    countries functions added

---
+++ src/utils/groupCountries.js
@@ -0,0 +1,27 @@
+export const filterCountriesByName = (countries, name) => {
+    return countries.filter(country => country.name.toLowerCase().includes(name.toLowerCase()))
+}
+
+const groupByLanguage = (countries) => {
+    const groupedCountries = countries.reduce((result, item) => {
+        result[item.languages[0].name] = [...result[item.languages[0].name] || [], item]
+        return result
+    })
+    return groupedCountries
+}
+
+const groupByContinent = (countries) => {
+    const groupedCountries = countries.reduce((result, item) => {
+        result[item.continent.name] = [...result[item.continent.name] || [], item]
+        return result
+    }, {})
+    return groupedCountries
+}
+
+export const groupBy = (countries, condition) => {
+    if (condition === 'continent') {
+        return groupByContinent(countries)
+    } else {
+        return groupByLanguage(countries)
+    }
+}
